d11dc1501b044309e596b9d97bf753b2deceb941
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2020-02-15 21:13:01 -0300

    Creando el archivo de rutas, contiene las rutas para Organizaciones, Niveles y Personas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,56 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('inicio');
+    });
+
+    //GRUPO DE RUTAS PARA CRUD ORGANIZACIONES
+    Route::resource('organizaciones', 'OrganizacionController');
+    Route::get('getOrganizacion/{id}' , [
+        'uses' => 'OrganizacionController@getOrganizacion'
+    ]);
+
+    //GRUPO DE RUTAS PARA CRUD NIVELES DEPARTAMENTALES
+    Route::resource('nivelesDepartamentales', 'NivelDepartamentoController');
+    Route::get('getNivelDepartamental{id}' , [
+        'uses' => 'NivelDepartamentoController@getNivelDepartamental'
+    ]);
+
+    //GRUPO DE RUTAS PARA CRUD NIVELES PUESTOS
+    Route::resource('nivelesPuestos', 'NivelPuestoController');
+    Route::get('getNivelPuesto/{id}' , [
+        'uses' => 'NivelPuestoController@getNivelPuesto'
+    ]);
+
+    //GRUPO DE RUTAS PARA CRUD PERSONAS
+    Route::resource('personas', 'PersonaController');
+    Route::get('getPersona/{id}' , [
+        'uses' => 'PersonaController@getPersona'
+    ]);
+
+    /*
+
+    //GRUPO DE RUTAS PARA CRUD DEPARTAMENTOS
+    Route::resource('departamentos', 'DepartamentoController');
+    Route::get('departamento/{id}' , [
+        'uses' => 'DepartamentoController@getDepartamento'
+    ]);
+
+    ROUTA CREATE QUE ACEPTA EL PARAMETRO
+    Route::get('departamentos/create/{id}' , [
+        'uses' => 'DepartamentoController@create'
+    ]);
+
+    Route::get('departamentos/{nivel_id}', function (\App\Departamento $departamento) {
+        return $departamento->only('id', 'nombre', 'dependeDe');
+    });
+
+
+    //GRUPO DE RUTAS PARA CRUD PUESTOS DE TRABAJOS
+    Route::resource('puestosTrabajos', 'PuestoTrabajoController');
+    Route::get('puestoTrabajo/{id}' , [
+        'uses' => 'PuestoTrabajoController@getPuestoTrabajo'
+    ]);
+
+     */
